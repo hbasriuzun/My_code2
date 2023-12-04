3f80c830aeadf3d8193984eb08663a43a6d65921
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2023-12-04 15:59:00 +0300

    .

---
+++ compatitive_programming/Something_About_Consecutive_Sum.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    long long n;
+    cin >> n;
+    long long sum = 0;
+    long long i = 1;
+    while(sum<=n){
+        sum = (i*(i+1))/2;
+        i++;
+    }
+    cout<<sum-n;
+	return 0;
+}
