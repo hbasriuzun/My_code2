93308924aa2a138463f6ceacdade03cdd3d2043e
Hasan Basri Uzun <102587414+hbasriuzun@users.noreply.github.com>
2024-01-06 16:24:49 +0300

    olgo

---
+++ compatitive_programming/AlgorithmCompetition_Winter_Camp_2024/yirmi_üc_ve_otuz_bes.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <algorithm>
+#include <array>
+#include <stack>
+#include <queue>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <string>
+#include <sstream>
+#include <cmath>
+#include <climits>
+#include <numeric>
+#include <iomanip>
+#include <bitset>
+#include <functional>
+#include <cassert>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <deque>
+#include <list>
+
+using namespace std;
+
+#define ll long long
+#define endl '\n'
+#define pb push_back 
+#define MOD 1000000007
+#define INF 1000000000
+#define MAXN 1000001
+const int N = 1e5+5;
+
+void solution(vector< vector<int> > arr,int n,int m){
+    vector< vector<int> > arr2;
+    arr2=arr;
+    for(int i=1; i<m; i++){
+        for(int j=1; j<n; j++){
+            if(arr[i][j] == 23 && (arr[i-1][j-1] != 23 && arr[i-1][j] != 23 && arr[i-1][j+1] != 23 && arr[i][j-1] != 23 && arr[i][j+1] != 23 && arr[i+1][j-1] != 23 && arr[i+1][j] != 23 && arr[i+1][j+1] != 23)){
+                arr2[i][j] = 35;
+            }
+            if(arr[i][j] == 35 && (arr[i-1][j-1] != 35 && arr[i-1][j] != 35 && arr[i-1][j+1] != 35 && arr[i][j-1] != 35 && arr[i][j+1] != 35 && arr[i+1][j-1] != 35 && arr[i+1][j] != 35 && arr[i+1][j+1] != 35)){
+                arr2[i][j] = 23;
+            }
+        }
+    }
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cout << arr2[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
+
+int main() {
+    int n,m;
+    cin >> n >> m;
+    vector < vector<int> > arr(n+2,vector<int>(m+2,0));
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            cin >> arr[i][j];
+        }
+    }
+
+    solution(arr,n,m);
+
+    return 0;
+}
